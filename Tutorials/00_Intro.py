112f1825cea7bba8369bf373e066231385ce1f8b
Hassam Ahmad <144841227+99Hassam99@users.noreply.github.com>
2025-03-26 04:03:41 +0500

    Delete Tutorials directory

--- Tutorials/00_Intro.py
+++
@@ -1,9 +0,0 @@
-from direct.showbase.ShowBase import ShowBase
-
-class MyGame(ShowBase):
-    def __init__(self):
-        ShowBase.__init__(self)
-        self.setBackgroundColor(0, 0, 0)  # Black background
-
-game = MyGame()
-game.run()
